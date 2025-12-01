4c39f8f5ca277f7ceebf2ab116cb7b49d45d86cf
Greg-Hawkridge <ghawkridge@gmail.com>
2025-12-01 12:00:45 +0000

    adds logging

--- solution_config/source_config/dashboard_ui/overload/handle_mqtt.jsx
+++ solution_config/source_config/dashboard_ui/overload/handle_mqtt.jsx
@@ -2,11 +2,13 @@ export const initial_state = { connected: false }
 
 export async function new_message_action(dispatch, queryClient, message) {
     if (message && message.topic.match("location_state/update")) {
+        console.log("update received on ", message.topic)
         queryClient.refetchQueries({ queryKey: ["state"] })
         queryClient.refetchQueries({ queryKey: ["state_at", { id: message?.payload?.location_link }] })
         queryClient.refetchQueries({ queryKey: ['history_for', { id: message.payload.item_id }] })
         queryClient.refetchQueries({ queryKey: ['events_at', { id: message?.payload?.location_link }] })
     } else if (message && (message.topic.match("location_state/entered")) || message.topic.match("location_state/exited")) {
+        console.log("entry received on ", message.topic)
         queryClient.refetchQueries({ queryKey: ["state"] })
         queryClient.refetchQueries({ queryKey: ["state_at", { id: message?.payload?.location_link }] })
         queryClient.refetchQueries({ queryKey: ['history_for', { id: message.payload.item_id }] })
