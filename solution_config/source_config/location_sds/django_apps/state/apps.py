35ee804717cc955edf5177eebd56836c3e51923a
Greg Hawkridge <ghawkridge@gmail.com>
2025-12-12 11:17:31 +0000

    adds ability to limit items shown in complete location (#58)
    Individual Products are hidden from a special location in the main dashboard view after a configurable number of days.
    
    Co-authored-by: tobyaharris <51968582+tobyaharris@users.noreply.github.com>

--- solution_config/source_config/location_sds/django_apps/state/apps.py
+++ solution_config/source_config/location_sds/django_apps/state/apps.py
@@ -1,8 +1,19 @@
 from django.apps import AppConfig
-from django.db.models.signals import post_init
+from django.db.models.signals import post_init, post_migrate
 
 class StateConfig(AppConfig):
     name = 'state'
 
     def ready(self):
-        pass
+        post_migrate.connect(create_default_settings, sender=self)
+
+def create_default_settings(sender, **kwargs):
+    from .models import Setting
+
+    default_settings = {
+        "completed_location": "loc@4",
+        "completed_duration_days": "7"
+    }
+
+    for key, value in default_settings.items():
+        Setting.objects.get_or_create(key=key, defaults={"value": value})
